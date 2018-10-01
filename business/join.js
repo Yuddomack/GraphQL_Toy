7af9ca1e34a80b60e16e09499e0a9d67b6fe6070
Yuddomack <darr880406@gmail.com>
2018-10-01 14:39:42 +0900

    edit join logic

--- business/join.js
+++ business/join.js
@@ -1,7 +1,43 @@
 module.exports = function(){
     const conn = require('../model/orientdb');
 
+    async function isExistId(id){
+      let sql = 'select id from users where id=:id';
+      const user = await conn.query(sql, {
+        params: {
+          id: id
+        }
+      });
+
+      return (user.length > 0) ? true : false;
+    }
+
+    async function joinUser(id, pwd){
+      if(await isExistId(id)) throw { name:'exist id', message:'id is existed' };
+
+      let sql = 'insert into users(id, pwd) values(:id, :pwd)';
+      const result = await conn.query(sql, {
+        params: {
+          id: id,
+          pwd: pwd
+        }
+      });
+
+      /*
+      [ { '@class': 'users',
+        '@type': 'd',
+        id: 'qwer',
+        pwd: 'qwer',
+        '@rid': RecordID { cluster: 35, position: 0 },
+        '@version': 1 } ]
+      */
+      //console.log(result[0]['@rid']);
+
+      return (result.length > 0) ? 200 : 400; // code 200
+    }
+
     return {
-      getConnName: function(){ return conn.name },
+      getConnName: function(){ return conn.name }, // <- db관련 상속받는 객체를 만들어서 공용메소드로 달아보자
+      joinUser: joinUser,
     };
 }();
