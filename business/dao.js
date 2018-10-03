3552e08b6f75997b6a7c052a31e7ba6767b91512
Yuddomack <darr880406@gmail.com>
2018-10-03 14:36:54 +0900

    add function for direct query

--- business/dao.js
+++ business/dao.js
@@ -1,4 +1,9 @@
 module.exports = {
+  query:async function(sql, params){
+    const conn = require('../model/orientdb');
+
+    return await conn.query(sql, params);
+  }
   join:require('./join'),
   login:require('./login'),
 };
