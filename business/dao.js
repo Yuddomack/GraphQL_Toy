a31d38659a15a898ab28cb450ed614aab9c37bec
Yuddomack <darr880406@gmail.com>
2018-10-03 16:47:58 +0900

    add function for check session

--- business/dao.js
+++ business/dao.js
@@ -1,10 +1,15 @@
 module.exports = {
-  query:async function(sql, params){
+  query: async function(sql, params){
     const conn = require('../model/orientdb');
 
     return await conn.query(sql, params);
   },
-  join:require('./join'),
-  login:require('./login'),
-  board:require('./board'),
+  ckSession: function(session){
+    if(!session.rid) throw {name:'invalid session', message:'please login again'};
+
+    return this;
+  },
+  join: require('./join'),
+  login: require('./login'),
+  board: require('./board'),
 };
